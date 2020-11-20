4c8c3b1ee1f93c0e4a3db3f9fe03121bfe41ca9a
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-20 08:20:35 -0800

    stub out reviews and search

--- src/App.js
+++ src/App.js
@@ -3,18 +3,53 @@ import './App.css';
 import SubmitReview from './SubmitReview/component/SubmitReview'
 import UnitInformationBox from './units/UnitInformationBox'
 
+//TODO ADD SEARCH CONTAINER AND REVIEWS COMPONENTS FROM OTHER REPOS
+
+function SearchContainer() {
+  return <br />
+}
+function Reviews() {
+  return <br />
+}
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      unit: undefined
+      screen:'main',
+      lastScreen:undefined,
+      unit: undefined,
+      query: undefined
     }
   }
+  handleChangeScreen =(newScreen) => {
+    this.setState({lastScreen: this.state.screen, screen:newScreen});
+  }
+  handleSearch = (query) => {
+    this.setState({query:query});
+    this.handleChangeScreen('search');
+  }
+  handleUnitSelection = (unit) => {
+    this.setState({unit:unit});
+    this.handleChangeScreen('main');
+  }
   render() {
+    let screenContent='';
+    if (this.state.screen === 'search') {
+      screenContent=<SearchContainer query={this.state.query} />
+    }else {
+      if (this.state.unit) {
+        screenContent=<div>
+                        <UnitInformationBox unit={this.state.unit} />
+                        <SubmitReview unit={this.state.unit} />
+                        <Reviews unit={this.state.unit} />
+                      </div>
+      }else{
+        screenContent=<br />
+      }
+    }
     return (
       <div className="container">
-      <UnitInformationBox unit={this.state.unit} />
-      <SubmitReview unit={this.state.unit} />
+        {screenContent}
       </div>
     );
   }
