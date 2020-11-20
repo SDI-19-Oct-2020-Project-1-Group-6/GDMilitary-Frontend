515dc272011a6c1ece544355d10d6cad902cfeb9
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-20 07:57:04 -0800

    add depends, update submodules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import SubmitReview from './SubmitReview/component/SubmitReview'
+import UnitInformationBox from './units/UnitInformationBox'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      unit: undefined
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+      <UnitInformationBox unit={this.state.unit} />
+      <SubmitReview unit={this.state.unit} />
+      </div>
+    );
+  }
 }
 
 export default App;
