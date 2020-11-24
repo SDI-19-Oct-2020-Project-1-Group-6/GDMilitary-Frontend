870beb8ad9789c778e4201b494fd8f0e2a4b1ddb
brandonjohnson21 <brandon.johnson.21@us.af.mil>
2020-11-24 12:05:21 -0800

    search and submit integration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import './App.css';
-import SubmitReview from './SubmitReview/component/SubmitReview'
+import SubmitReview from './submit/SubmitReview'
 import UnitInformationBox from './units/UnitInformationBox'
-import ReviewsDisplay from './DisplayReview/displays/src/ReviewsDisplay'
-//TODO ADD SEARCH CONTAINER AND REVIEWS COMPONENTS FROM OTHER REPOS
+//import ReviewsDisplay from './DisplayReview/displays/src/ReviewsDisplay'
+import SearchComponent from './search/SearchComponent'
+import Header from './search/Header'
 
-function SearchContainer() {
+function ReviewsDisplay(props) {
   return <br />
 }
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      screen:'main',
+      screen:'search',
       lastScreen:undefined,
       unit: undefined,
       query: undefined
@@ -21,10 +22,6 @@ class App extends React.Component {
   handleChangeScreen =(newScreen) => {
     this.setState({lastScreen: this.state.screen, screen:newScreen});
   }
-  handleSearch = (query) => {
-    this.setState({query:query});
-    this.handleChangeScreen('search');
-  }
   handleUnitSelection = (unit) => {
     this.setState({unit:unit});
     this.handleChangeScreen('main');
@@ -32,7 +29,7 @@ class App extends React.Component {
   render() {
     let screenContent='';
     if (this.state.screen === 'search') {
-      screenContent=<SearchContainer query={this.state.query} />
+      screenContent=<SearchComponent onChangeUnit={this.handleUnitSelection}/>
     }else {
       if (this.state.unit) {
         screenContent=<div>
@@ -44,9 +41,10 @@ class App extends React.Component {
         screenContent=<br />
       }
     }
-    // TODO some css styling
+    
     return (
-      <div className="container">
+      <div className="ui container" style={{marginTop : '10px' }}>
+      <Header onSearch={()=>this.handleChangeScreen('search')}/>
         {screenContent}
       </div>
     );
